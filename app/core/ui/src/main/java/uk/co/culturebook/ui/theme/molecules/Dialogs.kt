254d96d04bd4167db8da74626fb1fe14f0a9cd50
George Sigalas <georgesg97@gmail.com>
2023-02-19 20:56:17 +0000

    oops

--- app/core/ui/src/main/java/uk/co/culturebook/ui/theme/molecules/Dialogs.kt
+++ app/core/ui/src/main/java/uk/co/culturebook/ui/theme/molecules/Dialogs.kt
@@ -5,17 +5,22 @@ import android.view.ContextThemeWrapper
 import android.widget.DatePicker
 import android.widget.TimePicker
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import uk.co.culturebook.ui.R
 import uk.co.culturebook.ui.theme.AppTheme
+import uk.co.culturebook.ui.theme.mediumHeaderRoundedShape
 import uk.co.culturebook.ui.theme.surfaceColorAtElevation
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -131,7 +136,30 @@ fun DateTimeDialog(
                 }
             }
         })
+}
 
+@Composable
+@Preview(device = Devices.PIXEL_XL)
+fun ModalBottomSheet(
+    modifier: Modifier = Modifier,
+    onDismiss: () -> Unit = {},
+    content: @Composable () -> Unit = { Text(modifier = Modifier.padding(16.dp), text = "hello") }
+) {
+    Dialog(
+        onDismissRequest = onDismiss,
+        properties = DialogProperties(usePlatformDefaultWidth = false)
+    ) {
+        BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+            Surface(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter),
+                shape = mediumHeaderRoundedShape
+            ) {
+                content()
+            }
+        }
+    }
 }
 
 @Composable
