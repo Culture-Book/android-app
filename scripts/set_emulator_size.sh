ab7767a0317039268c8fef01263d0430d9981b85
milky <georgesg97@gmail.com>
2023-04-21 15:31:28 +0100

    tests pass 4?

--- scripts/set_emulator_size.sh
+++ scripts/set_emulator_size.sh
@@ -12,7 +12,7 @@ for CONFIG_FILE in $CONFIG_FILES; do
   # Check if the skin.name configuration exists in the file
   if grep -q "skin.name" "$CONFIG_FILE"; then
     # Replace the entire line with the new screen size
-    sed -i "s/^skin.name.*/skin.name = $SCREEN_SIZE/" "$CONFIG_FILE"
+    sed -i '' -e "s/^skin.name.*/skin.name = $SCREEN_SIZE/" "$CONFIG_FILE"
   else
     # Add the new screen size to the end of the file
     echo "skin.name = $SCREEN_SIZE" >> "$CONFIG_FILE"
@@ -21,7 +21,7 @@ for CONFIG_FILE in $CONFIG_FILES; do
   # Check if the hw.lcd.height configuration exists in the file
   if grep -q "hw.lcd.height" "$CONFIG_FILE"; then
     # Replace the entire line with the new LCD height
-    sed -i "s/^hw.lcd.height.*/hw.lcd.height = $LCD_HEIGHT/" "$CONFIG_FILE"
+    sed -i '' -e "s/^hw.lcd.height.*/hw.lcd.height = $LCD_HEIGHT/" "$CONFIG_FILE"
   else
     # Add the new LCD height to the end of the file
     echo "hw.lcd.height = $LCD_HEIGHT" >> "$CONFIG_FILE"
@@ -30,7 +30,7 @@ for CONFIG_FILE in $CONFIG_FILES; do
   # Check if the hw.lcd.width configuration exists in the file
   if grep -q "hw.lcd.width" "$CONFIG_FILE"; then
     # Replace the entire line with the new LCD width
-    sed -i "s/^hw.lcd.width.*/hw.lcd.width = $LCD_WIDTH/" "$CONFIG_FILE"
+    sed -i '' -e "s/^hw.lcd.width.*/hw.lcd.width = $LCD_WIDTH/" "$CONFIG_FILE"
   else
     # Add the new LCD width to the end of the file
     echo "hw.lcd.width = $LCD_WIDTH" >> "$CONFIG_FILE"
