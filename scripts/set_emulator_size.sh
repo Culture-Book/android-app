b8220eadf63fa95ed7c4172c9ecbbda039b63a86
milky <georgesg97@gmail.com>
2023-04-21 12:45:34 +0100

    test pass 3.5

--- scripts/set_emulator_size.sh
+++ scripts/set_emulator_size.sh
@@ -2,6 +2,8 @@
 
 # Set the desired screen size
 SCREEN_SIZE="1090x2340"
+LCD_HEIGHT="2340"
+LCD_WIDTH="1090"
 
 AVD_NAME="$1"
 
@@ -15,4 +17,22 @@ if grep -q "skin.name" "$CONFIG_FILE"; then
 else
   # Add the new screen size to the end of the file
   echo "skin.name = $SCREEN_SIZE" >> "$CONFIG_FILE"
+fi
+
+# Check if the hw.lcd.height configuration exists in the file
+if grep -q "hw.lcd.height" "$CONFIG_FILE"; then
+  # Replace the entire line with the new LCD height
+  sed -i "s/^hw.lcd.height.*/hw.lcd.height = $LCD_HEIGHT/" "$CONFIG_FILE"
+else
+  # Add the new LCD height to the end of the file
+  echo "hw.lcd.height = $LCD_HEIGHT" >> "$CONFIG_FILE"
+fi
+
+# Check if the hw.lcd.width configuration exists in the file
+if grep -q "hw.lcd.width" "$CONFIG_FILE"; then
+  # Replace the entire line with the new LCD width
+  sed -i "s/^hw.lcd.width.*/hw.lcd.width = $LCD_WIDTH/" "$CONFIG_FILE"
+else
+  # Add the new LCD width to the end of the file
+  echo "hw.lcd.width = $LCD_WIDTH" >> "$CONFIG_FILE"
 fi
